de6fadbad7ce3f6b2f07a6864bf50914a9c37a0f
Marcelo Theodoro <marcelotheodoro@outlook.com>
2018-07-24 13:24:30 -0300

    Fix validations rules and add timestamp field that coerce to now when it is empty

--- eventcapture/schema.py
+++ eventcapture/schema.py
@@ -1,19 +1,24 @@
+from datetime import datetime
+
+
+def coerce_to_now(timestamp=None):
+    if not timestamp or timestamp == 'empty':
+        return datetime.utcnow()
+    return timestamp
+
+
 EVENT = {
     'event': {
         'type': 'string',
         'minlength': 1,
-        'maxlength': 10,
+        'maxlength': 100,
         'required': True,
     },
     'user_id': {
-        'type': 'string',
-        'minlength': 1,
-        'maxlength': 10,
+        'type': 'integer',
     },
     'business_id': {
-        'type': 'string',
-        'minlength': 1,
-        'maxlength': 10,
+        'type': 'integer',
     },
     'session_id': {
         'type': 'string',
@@ -28,4 +33,10 @@ EVENT = {
     'segmentation': {
         'type': 'dict',
     },
+    'timestamp': {
+        'type': 'datetime',
+        'default': 'empty',
+        'coerce': coerce_to_now,
+    },
+
 }
